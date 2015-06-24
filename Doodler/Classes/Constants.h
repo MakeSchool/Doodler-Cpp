ecf55b049a0c5ea4916f7927265367d852b335f5
Daniel Haaser <thedapperdan@gmail.com>
2015-06-24 00:14:44 -0700

    Remove unused colors

--- Doodler/Classes/Constants.h
+++ Doodler/Classes/Constants.h
@@ -18,8 +18,6 @@ static const cocos2d::Color4F COLOR_BLUE   =  cocos2d::Color4F(101/255.0f, 174/2
 static const cocos2d::Color4F COLOR_GREEN  =  cocos2d::Color4F(115/255.0f, 200/255.0f, 175/255.0f, 1.0f);
 static const cocos2d::Color4F COLOR_YELLOW =  cocos2d::Color4F(240/255.0f, 185/255.0f, 72/255.0f, 1.0f);
 static const cocos2d::Color4F COLOR_RED    =  cocos2d::Color4F(242/255.0f, 108/255.0f, 76/255.0f, 1.0f);
-static const cocos2d::Color4F COLOR_BLACK  =  cocos2d::Color4F(41/255.0f, 40/255.0f, 35/255.0f, 1.0f);
-static const cocos2d::Color4F COLOR_GRAY   =  cocos2d::Color4F(183/255.0f, 179/255.0f, 170/255.0f, 1.0f);
 static const cocos2d::Color4F COLOR_WHITE  =  cocos2d::Color4F(245/255.0f, 244/255.0f, 240/255.0f, 1.0f);
 
 #endif
